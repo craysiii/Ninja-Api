c72edcf369a013c1d47b52b261c2b6f4937053c5
craysiii <charles@cray.io>
2023-11-14 23:28:11 -0800

    Implement Location endpoints

--- NinjaOne-Api.Library/Endpoints/Location.cs
+++ NinjaOne-Api.Library/Endpoints/Location.cs
@@ -3,8 +3,27 @@
 public partial class Client
 {
     // https://app.ninjarmm.com/apidocs-beta/core-resources/operations/getNodeCustomFields_1
-    // TODO: Implement GetLocationCustomFields
+    public async Task<IDictionary<string, JsonElement>> GetOrganizationLocationCustomFields(
+        int organizationId,
+        int locationId,
+        bool? withInheritance = null
+        )
+    {
+        var request = new RestRequest(
+            string.Format(Resource.OrganizationLocationCustomFields, organizationId, locationId)
+            );
+        if (withInheritance is not null) request.AddQueryParameter(Param.WithInheritance, withInheritance.ToString());
+
+        return await GetResource<IDictionary<string, JsonElement>>(request);
+    }
     
     // https://app.ninjarmm.com/apidocs-beta/core-resources/operations/updateNodeAttributeValues_2
-    // TODO: Implement UpdateLocationCustomFIelds
+    public async Task<bool> UpdateOrganizationLocationCustomFields(int organizationId, int locationId, Hashtable fields)
+    {
+        var request = new RestRequest(
+            string.Format(Resource.OrganizationLocationCustomFields, organizationId, locationId)
+            );
+
+        return await PostResource(request, fields, Method.Patch);
+    }
 }
